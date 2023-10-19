1816375b14fab8eeef6e26155f98b2098c4fe64e
alimasudd <alimasudd.dev@gmail.com>
2023-10-19 10:55:52 +0700

    declare varibles in login_controller.dart

--- lib/app/modules/login/controllers/login_controller.dart
+++ lib/app/modules/login/controllers/login_controller.dart
@@ -1,23 +1,13 @@
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class LoginController extends GetxController {
-  //TODO: Implement LoginController
 
-  final count = 0.obs;
-  @override
-  void onInit() {
-    super.onInit();
-  }
+  TextEditingController email = TextEditingController();
+  TextEditingController password = TextEditingController();
+  TextEditingController emailReset = TextEditingController();
+  var isObscure = true.obs;
+  var emailSent = false.obs;
 
-  @override
-  void onReady() {
-    super.onReady();
-  }
 
-  @override
-  void onClose() {
-    super.onClose();
-  }
-
-  void increment() => count.value++;
 }
