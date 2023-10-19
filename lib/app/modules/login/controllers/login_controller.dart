41e32bd144c28b6bfb7e70b977e0b3787fc0b9e5
alimasudd <alimasudd.dev@gmail.com>
2023-10-19 10:56:43 +0700

    create signInWithGoogle function

--- lib/app/modules/login/controllers/login_controller.dart
+++ lib/app/modules/login/controllers/login_controller.dart
@@ -10,4 +10,21 @@ class LoginController extends GetxController {
   var emailSent = false.obs;
 
 
+  signInWithGoogle() async {
+    // Trigger the authentication flow
+    final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
+    Get.back();
+    // Obtain the auth details from the request
+    final GoogleSignInAuthentication? googleAuth = await googleUser?.authentication;
+
+    // Create a new credential
+    final credential = GoogleAuthProvider.credential(
+      accessToken: googleAuth?.accessToken,
+      idToken: googleAuth?.idToken,
+    );
+    // Once signed in, return the UserCredential
+    return await FirebaseAuth.instance.signInWithCredential(credential);
+  }
+
+
 }
